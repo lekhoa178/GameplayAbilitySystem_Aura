61828a30d040b9a4b5c862fb7eb1b594f2186073
lekhoa178 <leanhkhoapl178@gmail.com>
2024-11-15 20:13:17 +0700

    23. Init Ability Actor Info

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -16,10 +16,12 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	GENERATED_BODY()
 public:
 	AAuraEnemy();
-	
+
+	//~ Begin Enemy Interface
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
+	//~ End Enemy Interface
 	
-	UPROPERTY(BlueprintReadOnly)
-	bool bHighlighted = false;
+protected:
+	virtual void BeginPlay() override;
 };
