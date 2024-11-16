33cbdc16249348bcf395ca63060adcb51f56addb
lekhoa178 <leanhkhoapl178@gmail.com>
2024-11-16 09:44:43 +0700

    32. Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+// Copyright KhoaLe
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "Blueprint/UserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
