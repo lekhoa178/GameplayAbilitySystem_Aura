ebbc8d004afd8f4ae2534223a228b7a38db7f9d7
lekhoa178 <leanhkhoapl178@gmail.com>
2024-11-20 22:14:24 +0700

    90. Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright KhoaLe
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
