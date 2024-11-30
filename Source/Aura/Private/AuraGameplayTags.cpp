ea6be9116da5f8baec9a0a56e218195f7e3d1f9b
lekhoa178 <leanhkhoapl178@gmail.com>
2024-11-30 20:23:25 +0700

    134. Set by Caller Magnitude

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -97,4 +97,9 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	GameplayTags.InputTag_4 = UGameplayTagsManager::Get().AddNativeGameplayTag(
 			FName("InputTag.4"),
 			FString("Input Tag for 4"));
+
+
+	GameplayTags.Damage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Damage"),
+		FString("Damage"));
 }
